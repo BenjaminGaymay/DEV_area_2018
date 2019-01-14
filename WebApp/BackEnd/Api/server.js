903caa0608d2927cb3caadb4f265c1ca3633248d
Enguerrand <enguerrand.arminjon@epitech.eu>
2019-01-14 13:11:19 +0100

    back & front init

---
+++ WebApp/BackEnd/Api/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {router} from "./router";
+import http from "http";
+import {rootDir} from "./constant";
+
+const app = express();
+
+app.use(express.static(rootDir + '/assets'));
+
+const server = http.createServer(app);
+server.listen(8081, function () {
+    console.log('Example app listening on port 8081 !')
+});
+
+router(app);
