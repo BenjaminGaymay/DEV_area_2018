eeee7cb69690fa429a9adf164b7c89d17866c3ab
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2019-01-23 14:12:17 +0100

    update Dockerfiles & add about.json

--- WebApp/BackEnd/Api/router.js
+++ WebApp/BackEnd/Api/router.js
@@ -1,5 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+
+function getUnixTime() {
+	return Date.now() / 1000 | 0;
+};
+
 export function router(app) {
     app.get('/', function (req, res) {
         res.send("coucou");
+    });
+
+    app.get('/about.json', (req, res) => {
+        const about = JSON.parse(fs.readFileSync('about.json', 'utf8'));
+
+        about.client.host = req.ip.split(':').pop();
+        about.server.current_time = getUnixTime();
+        res.send(about);
     });
 }
