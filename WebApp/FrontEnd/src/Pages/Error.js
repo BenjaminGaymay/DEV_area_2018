022404e1ac3d45520f3b0577fd503551544b9dc3
Benoit PIngris <benoit.pingris@epitech.eu>
2019-01-30 15:05:52 +0100

    404 update

--- WebApp/FrontEnd/src/Pages/Error.js
+++ WebApp/FrontEnd/src/Pages/Error.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Grid, Card, CardActionArea, CardMedia, CardContent, Typography } from '@material-ui/core';
 
 export default class Error extends React.Component {
     render() {
         return (
-            <div>
-                ERROR 404 NOT FOUND
-            </div>
+            <Grid container justify="center" alignContent="center" alignItems="center" style={{ height: '500px' }}>
+                <Card>
+                    <CardActionArea>
+                        <CardMedia
+                            component="img"
+                            alt="chap"
+                            image="https://media.giphy.com/media/gngO1gmBhS9na/giphy.gif"
+                            title="LoliloL"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                404
+                        </Typography>
+                            <Typography component="p">
+                                Omg page not found lol
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Grid>
         );
     }
 }
