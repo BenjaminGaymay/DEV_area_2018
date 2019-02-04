10cc372b02f8c825f0e510de4bad833afcde790b
Benoit PIngris <benoit.pingris@epitech.eu>
2019-02-04 11:40:27 +0100

    modify privateroute component guard

--- WebApp/FrontEnd/src/guard/PrivateRoute.js
+++ WebApp/FrontEnd/src/guard/PrivateRoute.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-
 import Context from "../context/context";
 
-class PrivateRouteConsumer extends React.Component {
-  render() {
-    if (!this.props.context.isLogged) {
-      return <Redirect to="/login" />;
-    }
-    return <Route {...this.props} />;
+const PrivateRouteConsumer = ({ context, props }) => {
+  if (!context.isLogged) {
+    return <Redirect to="/login" />;
   }
-}
+  return <Route {...props} />;
+};
 
 const PrivateRoute = props => {
   return (
     <Context.Consumer>
-      {context => <PrivateRouteConsumer context={context} />}
+      {context => <PrivateRouteConsumer context={context} props={props} />}
     </Context.Consumer>
   );
 };
