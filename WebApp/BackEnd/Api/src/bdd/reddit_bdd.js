0640d05fd7b4aba01f835b1ee3741490a79fc4f5
Enguerrand <enguerrand.arminjon@gmail.com>
2019-02-12 10:47:15 +0100

    add reddit http

--- WebApp/BackEnd/Api/src/bdd/reddit_bdd.js
+++ WebApp/BackEnd/Api/src/bdd/reddit_bdd.js
@@ -5,7 +5,7 @@ export async function updateAllSubscribedUsers(name, created, data) {
         data = data.replace(/'/g, "\\'");
         return query(`UPDATE link set config_action=JSON_SET(config_action, '$.created', '${created}'), updated=TRUE,
                       datas='${data}'
-                      WHERE subscribe_id = '3' 
+                      WHERE subscribe_id = '3' or subscribe_id = '4' 
                       AND updated=FALSE
                       AND JSON_CONTAINS(config_action, '"${name}"', '$.name')
                       AND (JSON_EXTRACT(config_action, '$.created') is NULL or JSON_EXTRACT(config_action, '$.created') < '${created}');`)
