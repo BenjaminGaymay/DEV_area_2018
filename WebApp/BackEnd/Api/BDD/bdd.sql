8f15c11c514f8a6762a54d6b3f6e23158dff2d4e
Enguerrand <enguerrand.arminjon@gmail.com>
2019-02-14 12:22:21 +0100

    new bdd

--- WebApp/BackEnd/Api/BDD/bdd.sql
+++ WebApp/BackEnd/Api/BDD/bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 13 Février 2019 à 09:42
+-- Généré le :  Jeu 14 Février 2019 à 12:22
 -- Version du serveur :  10.2.21-MariaDB-10.2.21+maria~stretch
 -- Version de PHP :  7.2.14-1+0~20190113100742.14+stretch~1.gbpd83c69
 
@@ -41,13 +41,20 @@ CREATE TABLE `link` (
 --
 
 INSERT INTO `link` (`id`, `user_id`, `updated`, `subscribe_id`, `config_action`, `config_reaction`, `datas`) VALUES
-(1, 1, 0, 1, '{\"skinName\": \"Moonrise\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', '{\"skinName\": \"Moonrise\", \"vBucks\": \"1200\" , \"url\": \"https://cdn.thetrackernetwork.com/cdn/fortnite/797C6306_large.png\"}'),
-(2, 1, 0, 2, '{\"skinName\": \"Sanctum\"}', '{\"method\": \"POST\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"skinName\": \"Sanctum\", \"vBucks\": \"1500\" , \"url\": \"https://cdn.thetrackernetwork.com/cdn/fortnite/13926359_large.png\"}'),
-(3, 1, 0, 3, '{\"name\": \"funny\", \"created\": \"1549993465\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', '{\"title\":\"*cries in native american*\",\"author\":\"Sean_Lock\",\"url\":\"https://i.redd.it/06uiu8n4z3g21.png\",\"created\":1549993465}'),
-(4, 1, 0, 4, '{\"name\": \"funny\", \"created\": \"1549993465\"}', '{\"method\": \"POST\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"title\":\"*cries in native american*\",\"author\":\"Sean_Lock\",\"url\":\"https://i.redd.it/06uiu8n4z3g21.png\",\"created\":1549993465}'),
-(5, 1, 0, 5, '{\"token\": \"azerty12345\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', NULL),
-(6, 1, 1, 6, '{\"platform\": \"pc\", \"pseudo\": \"RelaxasFr\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', '{\"accountId\":\"83aa13a9-5980-4e2a-b4bb-1c4247e6dcc0\",\"ratio\":\"1.59\",\"matches\":\"617\",\"kills\":\"958\",\"top1\":\"14\",\"winPourcentage\":\"2%\"}'),
-(7, 1, 1, 7, '{\"platform\": \"pc\", \"pseudo\": \"RelaxasFr\"}', '{\"method\": \"GET\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"accountId\":\"83aa13a9-5980-4e2a-b4bb-1c4247e6dcc0\",\"ratio\":\"1.59\",\"matches\":\"617\",\"kills\":\"958\",\"top1\":\"14\",\"winPourcentage\":\"2%\"}');
+(1, 1, 0, 72, '{\"skinName\": \"Moonrise\"}', '{\"to\": [\"poubelleapipoubelle@gmail.com\"]}', '{\"skinName\": \"Moonrise\", \"vBucks\": \"1200\" , \"url\": \"https://cdn.thetrackernetwork.com/cdn/fortnite/797C6306_large.png\"}'),
+(2, 1, 0, 71, '{\"skinName\": \"Sanctum\"}', '{\"method\": \"POST\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"skinName\": \"Sanctum\", \"vBucks\": \"1500\" , \"url\": \"https://cdn.thetrackernetwork.com/cdn/fortnite/13926359_large.png\"}'),
+(3, 1, 0, 22, '{\"name\": \"funny\", \"created\": \"1549993465\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', '{\"title\":\"*cries in native american*\",\"author\":\"Sean_Lock\",\"url\":\"https://i.redd.it/06uiu8n4z3g21.png\",\"created\":1549993465}'),
+(4, 1, 0, 21, '{\"name\": \"funny\", \"created\": \"1549993465\"}', '{\"method\": \"POST\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"title\":\"*cries in native american*\",\"author\":\"Sean_Lock\",\"url\":\"https://i.redd.it/06uiu8n4z3g21.png\",\"created\":1549993465}'),
+(5, 1, 0, 91, '{\"token\": \"azerty12345\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', NULL),
+(6, 1, 0, 82, '{\"platform\": \"pc\", \"pseudo\": \"RelaxasFr\"}', '{\"to\": \"poubelleapipoubelle@gmail.com\"}', '{\"accountId\":\"83aa13a9-5980-4e2a-b4bb-1c4247e6dcc0\",\"ratio\":\"1.59\",\"matches\":\"617\",\"kills\":\"958\",\"top1\":\"14\",\"winPourcentage\":\"2%\"}'),
+(7, 1, 0, 81, '{\"platform\": \"pc\", \"pseudo\": \"RelaxasFr\"}', '{\"method\": \"GET\", \"url\": \"https://hookb.in/QJL2wRV962t9r92dlVEY\", \"headers\" : null}', '{\"accountId\":\"83aa13a9-5980-4e2a-b4bb-1c4247e6dcc0\",\"ratio\":\"1.59\",\"matches\":\"617\",\"kills\":\"958\",\"top1\":\"14\",\"winPourcentage\":\"2%\"}'),
+(8, 1, 0, 1, NULL, '{\"to\": [\"benjamingaymay@gmail.com\"]}', NULL),
+(9, 1, 0, 3, NULL, '{\"access_token\": \"39aaa244d29216043d564f3b1563be995f7dac04\"}', NULL),
+(10, 1, 0, 4, NULL, '{\"access_token\": \"39aaa244d29216043d564f3b1563be995f7dac04\", \"username\": \"Robin-P\", \"repoName\": \"zappy\"}', NULL),
+(11, 1, 0, 20, '{\"access_token\": \"175327689916-FSHuPKouqILpAm63Yw1y_oM33X8\"}', NULL, '{\"trophies\":[]}'),
+(12, 1, 0, 30, '{\"access_token\": \"39aaa244d29216043d564f3b1563be995f7dac04\"}', NULL, '{\"repos\":[{\"name\":\"wenowTask\",\"url\":\"https://github.com/BenjaminGaymay/wenowTask\"},{\"name\":\"arcade\",\"url\":\"https://github.com/Robin-P/arcade\"},{\"name\":\"asm_minilibc\",\"url\":\"https://github.com/Robin-P/asm_minilibc\"},{\"name\":\"battleship\",\"url\":\"https://github.com/Robin-P/battleship\"},{\"name\":\"bsq\",\"url\":\"https://github.com/Robin-P/bsq\"},{\"name\":\"indie-studio\",\"url\":\"https://github.com/Robin-P/indie-studio\"},{\"name\":\"lemin\",\"url\":\"https://github.com/Robin-P/lemin\"},{\"name\":\"lemipc\",\"url\":\"https://github.com/Robin-P/lemipc\"},{\"name\":\"matchstick\",\"url\":\"https://github.com/Robin-P/matchstick\"},{\"name\":\"maths\",\"url\":\"https://github.com/Robin-P/maths\"},{\"name\":\"mon-cv\",\"url\":\"https://github.com/Robin-P/mon-cv\"},{\"name\":\"my_ftp\",\"url\":\"https://github.com/Robin-P/my_ftp\"},{\"name\":\"my_irc\",\"url\":\"https://github.com/Robin-P/my_irc\"},{\"name\":\"nanotekspice\",\"url\":\"https://github.com/Robin-P/nanotekspice\"},{\"name\":\"philosopher\",\"url\":\"https://github.com/Robin-P/philosopher\"},{\"name\":\"plazza\",\"url\":\"https://github.com/Robin-P/plazza\"},{\"name\":\"shell\",\"url\":\"https://github.com/Robin-P/shell\"},{\"name\":\"tetris\",\"url\":\"https://github.com/Robin-P/tetris\"},{\"name\":\"zappy\",\"url\":\"https://github.com/Robin-P/zappy\"}]}'),
+(13, 1, 0, 40, '{\"access_token\": \"39aaa244d29216043d564f3b1563be995f7dac04\"}', NULL, '{\"id\":\"444820323\",\"reason\":\"mention\",\"title\":\"erwann\",\"type\":\"Issue\",\"url\":\"https://github.com/Robin-P/zappy\"}'),
+(14, 1, 0, 50, '{\"access_token\": \"39aaa244d29216043d564f3b1563be995f7dac04\"}', NULL, '{\"id\":406774030,\"url\":\"https://github.com/Robin-P/zappy/issues/6\",\"title\":\"je tente un titre\"}');
 
 -- --------------------------------------------------------
 
@@ -68,33 +75,12 @@ CREATE TABLE `service` (
 INSERT INTO `service` (`id`, `filename`, `datas`) VALUES
 (1, 'fortnite', '{\"lastUpdate\": \"2019-02-13 09:27:12\"}'),
 (2, 'mail', NULL),
-(3, 'reddit', NULL),
-(15, 'http', NULL);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `subscribe`
---
-
-CREATE TABLE `subscribe` (
-  `id` int(11) NOT NULL,
-  `filename` varchar(40) NOT NULL,
-  `datas` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Contenu de la table `subscribe`
---
-
-INSERT INTO `subscribe` (`id`, `filename`, `datas`) VALUES
-(1, 'fortnite-shop-mail', NULL),
-(2, 'fortnite-shop-http', NULL),
-(3, 'reddit-mail', NULL),
-(4, 'reddit-http', NULL),
-(5, 'http-mail', NULL),
-(6, 'fortnite-stats-mail', NULL),
-(7, 'fortnite-stats-http', NULL);
+(3, 'imdb', 'Creed II'),
+(4, 'clashroyale', NULL),
+(5, 'reddit', NULL),
+(6, 'http', NULL),
+(7, 'leagueoflegends', NULL),
+(8, 'github', NULL);
 
 -- --------------------------------------------------------
 
@@ -171,14 +157,6 @@ ALTER TABLE `service`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Index pour la table `subscribe`
---
-ALTER TABLE `subscribe`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`filename`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
@@ -203,17 +181,12 @@ ALTER TABLE `user_tmp`
 -- AUTO_INCREMENT pour la table `link`
 --
 ALTER TABLE `link`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT pour la table `service`
 --
 ALTER TABLE `service`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT pour la table `subscribe`
---
-ALTER TABLE `subscribe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
